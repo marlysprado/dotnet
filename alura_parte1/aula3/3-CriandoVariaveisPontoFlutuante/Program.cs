ae6ca2d090dd0a636388ca727dd1afb77695240e
Marly Prado <mprado@ciandt.com>
2021-09-05 18:14:55 -0300

    feat: adds 3-CriandoVariaveisPontoFlutuante

---
+++ alura_parte1/aula3/3-CriandoVariaveisPontoFlutuante/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _3_CriandoVariaveisPontoFlutuante
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 3 Criando variáveis ponto flutuante");
+
+            double salario = 1234.56;
+            Console.WriteLine(salario);
+
+            double salario2 = 12 / 3;
+            Console.WriteLine("12 / 3" + salario2);
+
+            salario2 = 12 / 3.0;
+            Console.WriteLine("12 / 3.0" + salario2);
+
+            Console.WriteLine("A execução acabou. Tecle enter para sair ...");
+            Console.ReadLine();
+        }
+    }
+}
