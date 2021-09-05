5e2f5160eaab2f776eaae82db4b51afd97627066
Marly Prado <mprado@ciandt.com>
2021-09-05 18:14:15 -0300

    feat: adds 2-CriandoVariaveis

---
+++ alura_parte1/aula3/2-CriandoVariaveis/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _2_CriandoVariaveis
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Executando projeto 2 - Criando Variáveis");
+
+      int idade = 34;
+      Console.WriteLine(idade);
+
+      idade = 3 + 4;
+      Console.Write(idade);
+
+      idade = (3 + 4) * 1;
+      Console.WriteLine("Sua idade é " + idade + '!');
+
+      Console.WriteLine("Execução finalizada. Tecle enter para sair...'");
+      Console.ReadLine();
+    }
+  }
+}
