7f0f7d5858e11352ae5f7b608b9b788fb711740f
Marly Prado <mprado@ciandt.com>
2021-09-05 18:15:38 -0300

    feat: adds 4_ConversoesEOutrosTiposNumericos

---
+++ alura_parte1/aula3/4_ConversoesEOutrosTiposNumericos/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _4_ConversoesEOutrosTiposNumericos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 4");
+
+            // 16 bits
+            short quantidadeProdutos = 15000;
+            Console.WriteLine(quantidadeProdutos);
+
+            // menor precisão
+            float altura = 1.80f;
+            Console.WriteLine(altura);
+
+            double salario = 123.455555;
+            Console.WriteLine(salario);
+
+            // 32 bits
+            int salarioEmInteiro = (int)salario;
+            Console.WriteLine(salarioEmInteiro);
+
+            // 64 bits
+            long idade = 1000000000000000000;
+            Console.WriteLine(idade);
+
+            Console.ReadLine();
+        }
+    }
+}
