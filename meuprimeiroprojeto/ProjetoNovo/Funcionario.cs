94a5ecafd8d5f2516ce101b04bab7bc779887b16
Marly Prado <mprado@ciandt.com>
2021-09-05 14:57:39 -0300

    feat: adds meuprimeiroprojeto project

---
+++ meuprimeiroprojeto/ProjetoNovo/Funcionario.cs
@@ -0,0 +1,9 @@
+namespace ProjetoNovo
+{
+  internal class Funcionario
+  {
+    public Funcionario()
+    {
+    }
+  }
+}
---
+++ meuprimeiroprojeto/ProjetoNovo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ProjetoNovo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor = 10;
+            Console.WriteLine("Teste!");
+            Funcionario funcionario = new Funcionario();
+            Console.WriteLine(valor);
+        }
+    }
+}
